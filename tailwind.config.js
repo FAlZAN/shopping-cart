04478334c02876fd11142a5b8f1c28f04d491c5b
Faizan Shaikh <faizanshaikh565@gmail.com>
2023-08-24 12:37:12 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  daisyui: {
+    darkTheme: false,
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+};
