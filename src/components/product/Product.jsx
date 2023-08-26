5f95b773b1a81b15734f8df433c3f7cf0a57a165
Faizan Shaikh <faizanshaikh565@gmail.com>
2023-08-26 14:25:06 +0530

    product card ui fixed.

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../features/cart/cart";
+
 function Product({ product }) {
+  const dispatch = useDispatch();
+
+  function handleAddToCart(item) {
+    console.log(item);
+    dispatch(addToCart({ ...item, quantityInCart: 1 }));
+  }
   return (
-    <div className="w-64 p-5 border border-black/50  flex flex-col justify-between items-center gap-5">
+    <div className="p-3 lg:p-5 border border-black/50  flex flex-col justify-between items-center gap-5">
       <figure className="w-full h-full bg-white  flex justify-center items-center">
-        <img width={120} src={product.image} alt={product.title} />
+        <img className="w-28" src={product.image} alt={product.title} />
       </figure>
 
       <div className="w-full  flex flex-col gap-2">
-        <p className="card-title line-clamp-2">{product.title}</p>
-        <p className="card-title">$ {product.price}</p>
+        <p className="line-clamp-1 sm:line-clamp-2">{product.title}</p>
+        <p className="">$ {product.price}</p>
       </div>
-      <button className="btn w-full rounded-none">Add to cart</button>
+      <button
+        className="btn w-full rounded-none"
+        onClick={() => handleAddToCart(product)}
+      >
+        Add to cart
+      </button>
     </div>
   );
 }
