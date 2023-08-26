27e9382efc3c59a79fe4654bab6e01c4696458de
Faizan Shaikh <faizanshaikh565@gmail.com>
2023-08-26 22:03:40 +0530

    product list added to homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+// query hook
+import { useGetProductsQuery } from "../../services/products";
+// component
+import Product from "../../components/product/Product";
+
+import { useSelector } from "react-redux";
 
 function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
+  const { data: products, error, isLoading } = useGetProductsQuery("limit=10");
+  const cart = useSelector((state) => state.cart.items);
+  console.log(cart);
+
+  let content;
+
+  if (isLoading) {
+    content = (
+      <div className="h-screen  flex justify-center items-center">
+        <span className="loading loading-spinner loading-lg"></span>
+      </div>
+    );
+  } else {
+    content = (
+      <div className="m-auto p-3 sm:p-5 max-w-7xl  grid xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 xs:gap-3">
+        {products?.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </div>
+    );
+  }
+
+  return <div>{content}</div>;
 }
 
 export default Home;
