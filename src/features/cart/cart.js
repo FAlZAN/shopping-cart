1eda8974004ff89596df2562d159f0ac878c545e
Faizan Shaikh <faizanshaikh565@gmail.com>
2023-08-28 11:39:08 +0530

    added logic for cart functionality.

--- src/features/cart/cart.js
+++ src/features/cart/cart.js
@@ -17,8 +17,35 @@ export const cartSlice = createSlice({
         itemExists.quantityInCart += 1;
       }
     },
+    increaseProductQuantity: (state, action) => {
+      const product = state.items.find((item) => item.id === action.payload.id);
+      product.quantityInCart += 1;
+    },
+    decreaseProductQuantity: (state, action) => {
+      const product = state.items.find((item) => item.id === action.payload.id);
+
+      if (product.quantityInCart > 1) {
+        product.quantityInCart -= 1;
+      } else {
+        const newItems = state.items.filter(
+          (item) => item.id !== action.payload.id
+        );
+        state.items = newItems;
+      }
+    },
+    removeFromCart: (state, action) => {
+      const newItems = state.items.filter(
+        (item) => item.id !== action.payload.id
+      );
+      state.items = newItems;
+    },
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const {
+  addToCart,
+  increaseProductQuantity,
+  decreaseProductQuantity,
+  removeFromCart,
+} = cartSlice.actions;
 export default cartSlice.reducer;
