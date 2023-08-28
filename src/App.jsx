c273ab45e1530bc79e510bb8632893352472f0e4
Faizan Shaikh <faizanshaikh565@gmail.com>
2023-08-28 19:17:24 +0530

    added logic to set cart state from persisted cart state locally.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // component
 import Navbar from "./components/navbar/Navbar";
@@ -6,7 +6,20 @@ import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 
+import { useDispatch } from "react-redux";
+import { setInitialState } from "./features/cart/cart";
+
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const localCart = localStorage.getItem("cart");
+
+    if (localCart !== null) {
+      dispatch(setInitialState(JSON.parse(localCart)));
+    }
+  }, []);
+
   return (
     <div className="App">
       <BrowserRouter>
