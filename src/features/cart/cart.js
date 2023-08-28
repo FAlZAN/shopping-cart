c998103ada01220623dacfc7dddb193a638c19d3
Faizan Shaikh <faizanshaikh565@gmail.com>
2023-08-28 19:15:39 +0530

    added reducer to set initial state.

--- src/features/cart/cart.js
+++ src/features/cart/cart.js
@@ -13,24 +13,29 @@ export const cartSlice = createSlice({
 
       if (!itemExists) {
         state.items = [...state.items, action.payload];
+        localStorage.setItem("cart", JSON.stringify(state.items));
       } else {
         itemExists.quantityInCart += 1;
+        localStorage.setItem("cart", JSON.stringify(state.items));
       }
     },
     increaseProductQuantity: (state, action) => {
       const product = state.items.find((item) => item.id === action.payload.id);
       product.quantityInCart += 1;
+      localStorage.setItem("cart", JSON.stringify(state.items));
     },
     decreaseProductQuantity: (state, action) => {
       const product = state.items.find((item) => item.id === action.payload.id);
 
       if (product.quantityInCart > 1) {
         product.quantityInCart -= 1;
+        localStorage.setItem("cart", JSON.stringify(state.items));
       } else {
         const newItems = state.items.filter(
           (item) => item.id !== action.payload.id
         );
         state.items = newItems;
+        localStorage.setItem("cart", JSON.stringify(state.items));
       }
     },
     removeFromCart: (state, action) => {
@@ -38,6 +43,10 @@ export const cartSlice = createSlice({
         (item) => item.id !== action.payload.id
       );
       state.items = newItems;
+      localStorage.setItem("cart", JSON.stringify(state.items));
+    },
+    setInitialState: (state, action) => {
+      state.items = action.payload;
     },
   },
 });
@@ -47,5 +56,6 @@ export const {
   increaseProductQuantity,
   decreaseProductQuantity,
   removeFromCart,
+  setInitialState,
 } = cartSlice.actions;
 export default cartSlice.reducer;
