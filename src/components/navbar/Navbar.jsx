dc09209b90c68636a80a001074bee8842cbce40a
Faizan Shaikh <faizanshaikh565@gmail.com>
2023-08-29 14:35:24 +0530

    implemented items count in cart badge.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,39 +1,30 @@
-import React, { useEffect, useState } from "react";
-import {
-  ShoppingBagIcon,
-  MoonIcon,
-  SunIcon,
-} from "@heroicons/react/24/outline";
+import React from "react";
+import { ShoppingBagIcon } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
 
+import { useSelector } from "react-redux";
+import getTotalItemsInCart from "../../util/getTotalItemsInCart";
+
 function Navbar() {
-  const [theme, setTheme] = useState("light");
-
-  useEffect(() => {
-    document.getElementsByTagName("html")[0].setAttribute("data-theme", theme);
-  }, [theme]);
-
-  function toggleTheme() {
-    setTheme(theme === "dark" ? "light" : "dark");
-  }
+  const cart = useSelector((state) => state.cart.items);
+  const totalItemsInCart = getTotalItemsInCart(cart);
 
   return (
     <header className="flex justify-between">
       <div className="navbar bg-base-100 p-5 border-b border-gray-200">
         <div className="flex-1">
           <Link to="/" className="navbar-start font-bold text-3xl">
-            Shop
+            StopNShop
           </Link>
         </div>
 
-        <div className="flex justify-center items-center gap-8">
-          <label htmlFor="themeToggle" className="swap swap-rotate">
-            <input type="checkbox" id="themeToggle" onClick={toggleTheme} />
-            <MoonIcon className="swap-off w-9" />
-            <SunIcon className="swap-on w-9" />
-          </label>
-
-          <Link to="/cart">
+        <div>
+          <Link to="/cart" className="indicator">
+            {totalItemsInCart > 0 && (
+              <span className="indicator-item badge badge-neutral badge-sm">
+                {totalItemsInCart}
+              </span>
+            )}
             <ShoppingBagIcon className="w-9 pb-1" />
           </Link>
         </div>
