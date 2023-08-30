cce51cd136ee93f91f035db8529c67817e8fdf03
Faizan Shaikh <faizanshaikh565@gmail.com>
2023-08-30 22:42:08 +0530

    added order details reducerto redux store.

--- src/store.js
+++ src/store.js
@@ -4,11 +4,13 @@ import { setupListeners } from "@reduxjs/toolkit/query";
 import { productsApi } from "./services/products";
 
 import cartReducer from "./features/cart/cart";
+import orderReducer from "./features/order-details/orderDetails";
 
 export const store = configureStore({
   reducer: {
     [productsApi.reducerPath]: productsApi.reducer,
     cart: cartReducer,
+    orderDetails: orderReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(productsApi.middleware),
